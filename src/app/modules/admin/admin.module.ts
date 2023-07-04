703f0b2df7a98b773fe6c8c9134cebae4d183d7a
Soujanaya Sambanni <soujanayasambannibldea@gmail.com>
2023-07-04 15:13:37 +0530

    Initial Commit

---
+++ src/app/modules/admin/admin.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { AdminRoutingModule } from './admin-routing.module';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { ForYouComponent } from './components/for-you/for-you.component';
+import { GetCarComponent } from './components/get-car/get-car.component';
+import { PolicyComponent } from './components/policy/policy.component';
+import { HomeComponent } from './components/home/home.component';
+
+
+@NgModule({
+  declarations: [
+    HeaderComponent,
+    FooterComponent,
+    ForYouComponent,
+    GetCarComponent,
+    PolicyComponent,
+    HomeComponent
+  ],
+  imports: [
+    CommonModule,
+    AdminRoutingModule
+  ]
+})
+export class AdminModule { }
